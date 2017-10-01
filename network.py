3ffe4c451fc66a1a85c3e86d79e43f8998f0e6dd
bebo <beshoy@hotmail.com>
2017-10-01 14:45:47 -0700

    Code cleaning

--- network.py
+++ network.py
@@ -6,13 +6,17 @@
 # 3. Have a look on his solution vs mine 
 # 4. May start a mindmap summarizing my solution techniques and steps 
 # 5. Try pre-trained networks like (ImageNet - Inception ...) 
+# 6. Different levels of logging 
+# 7. Ambitious dream: what about strategy pattern to support different network architectures !!! :) 
 # 
 # Experiments: 
 # -------------
 # 1. Try the network with and without color 
 #######################################################################
 
+###########################################
 # --- Imports ---
+###########################################
 import sys
 sys.path.insert(0, '~/Desktop/TimeProfiler')
 from profiler       import Profiler
@@ -24,73 +28,45 @@ from keras.datasets import cifar10
 from keras.utils    import np_utils 
 import numpy 
 
+
+###########################################
 # from skimage import color 
+###########################################
 from PIL            import Image 
 from matplotlib     import pyplot
 
 timer.EndTime()
 timer.DeltaTime() 
 print('Log: end imports')
+
+###########################################
 # --- Load data --- 
+###########################################
 (X_train, Y_train) , (X_test, Y_test) = cifar10.load_data() 
 
 
-# img = color.rgb2gray(X_train[0]) 
-"""
-imgCount = 10
-img = Image.fromarray(X_train[imgCount])
-img = img.convert('1')
-"""
-# --- Show Image --- 
-"""
-pyplot.subplot(211) 
-pyplot.imshow(X_train[imgCount])
-pyplot.subplot(212) 
-pyplot.imshow(img)
-pyplot.show()
-"""
-
+###########################################
 # --- Fixed seed number for reproducibility --- 
+###########################################
 print('Log: start seed definition')
 seed = 7 
 numpy.random.seed(seed)
 print('Log: end seed definition')
+
+
 ###########################################
-# --- DEBUG --- 
-"""
-X_train_gray = numpy.ndarray(shape=(32, 32))
-for imageIndex in range(0, 10): 
-	img = Image.fromarray(X_train[imageIndex])
-	# X_train_gray.append(img.convert('1'))
-	X_train_gray = numpy.vstack([X_train_gray, img.convert('1')])
-X_train_gray = X_train_gray.reshape(11, 1, 32, 32)
-print("Finished Processing ... ")
-"""
-########################################### 
-
-
 # --- Convert color images into gray ones --- 
+###########################################
 print('Log: start training images conversion')
 timer.StartTime()
 X_train_gray = numpy.ndarray(shape=(32, 32))
 # for imageIndex in range(0, X_train.shape[0]): 
 # TODO: handling the dimension of the numpy array is pretty bad, find a better solution 
+# TODO: numpy create an initial record, I need to remove it 
 X_training_sample_size = 10
 for imageIndex in range(0, X_training_sample_size): 
 	img = Image.fromarray(X_train[imageIndex])
 	img = img.convert('1')
-	###########################################
-	# --- DEBUG --- 
-	# print('Log: X_train_gray dimensions= {} {} {}'.format(X_train_gray.shape[0], X_train_gray.shape[1], X_train_gray.shape[2]))
-	pyplot.subplot(111)
-	pyplot.imshow(img)
-	pyplot.show()
-
-	###########################################
-
-	# X_train_gray = numpy.vstack([X_train_gray, img.convert('1')])
-	# X_train_gray = numpy.concatenate([X_train_gray, img.convert('1')])
-	# X_train_gray = numpy.dstack([X_train_gray, img.convert('1')])
 	X_train_gray = numpy.dstack([X_train_gray, img])
 	print("Log: training image number = {}".format(imageIndex))
 X_train_gray = numpy.swapaxes(X_train_gray, 1, 2)
@@ -114,30 +90,29 @@ for imageIndex in range(0, X_training_sample_size/5):
 timer.EndTime()     
 timer.DeltaTime()
 print('Log: end testing image conversion')
-###########################################
-# --- DEBUG --- 
-print('Log: X_train_gray dimensions= {} {} {}'.format(X_train_gray.shape[0], X_train_gray.shape[1], X_train_gray.shape[2]))
-pyplot.subplot(111)
-pyplot.imshow(X_train_gray[1])
-pyplot.show()
-
-###########################################
-
-# --- Reshape to be [sample][channel][width][height] 
-# X_train_gray = X_train_gray.reshape(X_train_gray.shape[0], 1, 32, 32) 
-# X_test_gray  = X_test_gray.reshape(X_test_gray.shape[0], 1 , 32, 32)
 
 print('Log: start input manipulation')
 timer.StartTime()
+
+
+###########################################
+# --- Reshape to be [sample][channel][width][height] 
+###########################################
 # TODO: this is a temp solution, the added one needs to removed, because the first item in the array is fake and needs to be removed 
 X_train_gray = X_train_gray.reshape(X_training_sample_size +1, 1, 32, 32) 
 X_test_gray  = X_test_gray.reshape(X_training_sample_size/5 +1, 1 , 32, 32)
 
+
+###########################################
 # --- Normalization --- 
+###########################################
 X_train_gray = X_train_gray/255 
 X_test_gray  = X_test_gray/255 
 
+
+###########################################
 # --- One hot encoding --- 
+###########################################
 Y_train     = np_utils.to_categorical(Y_train)
 Y_test      = np_utils.to_categorical(Y_test) 
 num_classes = Y_test.shape[1]
@@ -150,8 +125,6 @@ timer.EndTime()
 timer.DeltaTime() 
 print('Log: end input manipulation')
 
-
-		
 
 
 
